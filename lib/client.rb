1bd807d4fe0fee34ea5fb071c30405c05c3516db
Diego Suarez <diego.spb@gmail.com>
2016-12-09 10:38:17 -0800

    add all, save and == override specs and methods

--- lib/client.rb
+++ lib/client.rb
@@ -1,3 +1,33 @@
-class Stylist
-  
+class Client
+  attr_reader(:name, :next_appointment, :id, :stylist_id)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name, nil)
+    @next_appointment = attributes.fetch(:next_appointment, nil)
+    @stylist_id = attributes.fetch(:stylist_id, 0)
+    @id = attributes.fetch(:id, nil)
+  end
+
+  define_method(:save) do
+    id = DB.exec("INSERT INTO clients (name, next_appointment, stylist_id ) VALUES ('#{@name}', '#{@next_appointment}', #{@stylist_id}) RETURNING id;")
+    @id = id.first.fetch("id").to_i()
+  end
+
+  define_singleton_method(:all) do
+    results = DB.exec("SELECT * FROM clients;")
+    clients = []
+    results.each() do |result|
+      id = result.fetch("id").to_i
+      name = result.fetch("name")
+      next_appointment = result.fetch("next_appointment")
+      stylist_id = result.fetch("stylist_id").to_i
+      clients.push(Client.new({:name => name, :next_appointment => next_appointment, :stylist_id => stylist_id, :id => id}))
+    end
+    clients
+  end
+
+  define_method(:==) do |other_client|
+    self.name.==(other_client.name).&(self.id.==(other_client.id)).&(self.next_appointment.==(other_client.next_appointment)).&(self.stylist_id.==(other_client.stylist_id))
+  end
+
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,1 +1,39 @@
 require('spec_helper')
+
+
+describe(Client) do
+
+  describe("#initialize") do
+    it "will check initial parameters" do
+      test_client = Client.new({:name => "Diego", :next_appointment => "2016-12-12 14:00:00"})
+      expect(test_client.name).to(eq('Diego'))
+      expect(test_client.next_appointment).to(eq('2016-12-12 14:00:00'))
+    end
+  end
+  describe('#save') do
+    it "save it to database" do
+      test_client = Client.new({:name => "Diego", :next_appointment => "2016-12-12 14:00:00"})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+  describe('.all') do
+    it "select all clients from database" do
+      test_client1 = Client.new({:name => "Diego", :next_appointment => "2016-12-12 14:00:00"})
+      test_client1.save()
+      test_client2 = Client.new({:name => "Kika", :next_appointment => "2016-12-13 14:00:00"})
+      test_client2.save()
+      expect(Client.all()).to(eq([test_client1, test_client2]))
+    end
+  end
+  describe('#==')do
+    it "override equals" do
+      test_client1 = Client.new({:name => "Diego", :next_appointment => "2016-12-12 14:00:00"})
+      test_client2 = Client.new({:name => "Diego", :next_appointment => "2016-12-12 14:00:00"})
+      expect(test_client1).to(eq(test_client2))
+
+    end
+  end
+
+
+end
