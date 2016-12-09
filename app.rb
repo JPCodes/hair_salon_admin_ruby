097296b1a164a6338a3f6c9b7a5c2cc536289c3b
Diego Suarez <diego.spb@gmail.com>
2016-12-09 13:33:22 -0800

    first integration spec, visit home

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/project')
+require('./lib/client')
+require('./lib/stylist')
 also_reload('lib/**/*.rb')
+require('pg')
+
+DB = PG.connect({:dbname => 'hair_salon_test'})
 
 
 get('/') do
   erb(:index)
+end
+
+get('/stylists') do
+  erb(:stylists)
+end
+
+get('/clients') do
+  erb(:clients)
+end
+get('/stylist_new') do
+  erb(:stylist_form)
 end
