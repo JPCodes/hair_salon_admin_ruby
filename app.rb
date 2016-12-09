b243a3a09639c7987ffaf62349b5bcc3f6df0a89
Diego Suarez <diego.spb@gmail.com>
2016-12-09 13:50:56 -0800

    integration spec to add new stylist

--- app.rb
+++ app.rb
@@ -13,12 +13,25 @@ get('/') do
 end
 
 get('/stylists') do
+  @stylists = Stylist.all()
   erb(:stylists)
 end
 
 get('/clients') do
   erb(:clients)
 end
+
 get('/stylist_new') do
   erb(:stylist_form)
+end
+
+post('/stylist_new') do
+  name = params.fetch('name')
+  specialty = params.fetch('specialty')
+  title = params.fetch('title')
+  image = params.fetch('image')
+  bio = params.fetch('bio')
+  Stylist.new({:name => name, :specialty => specialty, :title => title, :image => image, :bio => bio }).save()
+  @stylists = Stylist.all()
+  erb(:stylists)
 end
--- spec/project_integration_spec.rb
+++ spec/project_integration_spec.rb
@@ -3,9 +3,22 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the home path', {:type => :feature}) do
-  it('') do
+describe('HomePath', {:type => :feature}) do
+  it('checks the home path') do
     visit('/')
     expect(page).to have_content("Stylists")
+  end
+end
+describe('Stylists', {:type => :feature}) do
+  it('Add a new stylist') do
+    visit('/stylists')
+    click_link('Add a new Stylist here')
+    fill_in("name", :with=> "Stylist1")
+    fill_in("specialty", :with=> "Long-hair")
+    fill_in("title", :with=> "Senior-stylist")
+    fill_in("image", :with=> "www.place.com/image.jpg")
+    fill_in("bio", :with=> "Somebio")
+    click_button('Add Stylist')
+    expect(page).to have_content("Stylist1")
   end
 end
